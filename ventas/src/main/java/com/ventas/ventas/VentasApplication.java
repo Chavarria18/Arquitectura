a5ac3d2f035c18eaf1fdb8c23a1f43de1e60496d
Chavarria18 <chavarria181386@unis.edu.gt>
2021-09-14 15:07:16 -0600

    siu

--- ventas/src/main/java/com/ventas/ventas/VentasApplication.java
+++ ventas/src/main/java/com/ventas/ventas/VentasApplication.java
@@ -1,6 +1,4 @@
 package com.ventas.ventas;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
